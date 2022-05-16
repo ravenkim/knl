17850615a9e7e1768e70f428ee37fb8234c2d21a
changwon <hamin3822@gmail.com>
2022-05-16 22:26:28 +0900

    scan method 수정
    + OPTIONS 메소드 사용하여 allow 노출 시 해당 값을 표기하도록 함

--- scan_method.py
+++ scan_method.py
@@ -10,18 +10,23 @@ def scan_method():
     res_put = requests.put(scan_url)
     res_delete = requests.delete(scan_url)
 
-    print("\n사용중인 메소드\n")
-    if res_option.status_code == 200 : 
-        print("웹사이트 header 정보\n",res_option.headers)
+    print("\n사용중인 method\n")
+    if res_option.status_code == 200 :
+        try :  
+            if res_option.headers['allow'] :
+                print(res_option.headers['allow'],"사용")
+        except :
+            print("웹사이트 header 정보\n",res_option.headers)
+            if res_head.status_code == 200 : print("HEAD 사용")
+            else : print("응답코드",res_head.status_code," HEAD 미사용")
+            if res_get.status_code == 200 : print("GET 사용")
+            else : print("응답코드 ",res_head.status_code," GET 미사용")
+            if res_post.status_code == 200 : print("POST 사용")
+            else : print("응답코드 ",res_head.status_code," POST 미사용")
+            if res_put.status_code == 200 : print("PUT 사용")
+            else : print("응답코드 ",res_head.status_code," PUT 미사용")
+            if res_delete.status_code == 200 : print("DELETE 사용")
+            else : print("응답코드 ",res_head.status_code," DELETE 미사용")
     else :
         print("사이트 헤더 정보를 읽어올 수 없음")
-    if res_head.status_code == 200 : print("HEAD 사용")
-    else : print("응답코드",res_head.status_code," HEAD 미사용")
-    if res_get.status_code == 200 : print("GET 사용")
-    else : print("응답코드 ",res_head.status_code," GET 미사용")
-    if res_post.status_code == 200 : print("POST 사용")
-    else : print("응답코드 ",res_head.status_code," POST 미사용")
-    if res_put.status_code == 200 : print("PUT 사용")
-    else : print("응답코드 ",res_head.status_code," PUT 미사용")
-    if res_delete.status_code == 200 : print("DELETE 사용")
-    else : print("응답코드 ",res_head.status_code," DELETE 미사용")
+   
