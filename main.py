325a2c152f158df99b5712e03290b96a03377888
changwon <hamin3822@gmail.com>
2022-05-17 12:37:56 +0900

    bug fix
    * scan method bug fix
    - 문구 수정

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     print("\nWeb-vulnerability-scan-tool ver.0.1 - KNL\n")
     while(True):
         try:
-            print("사용할 기능을 선택 \n")
+            print("사용할 기능 선택 \n")
             print("1. bruteforce  2. 웹 서비스 메소드 점검  3. Injection(구현중, 미동작) 4. XSS(구현예정)")
             selec_modul = int(input("입력 : "))
             if selec_modul == 1 : 
@@ -23,7 +23,9 @@ if __name__ == "__main__":
             
             answer = input("\n\n프로그램을 종료 하시겠습니까? [Y/N] \n입력 : ")
             if answer == 'Y' or answer == 'y' : break
-            elif answer == 'N' or answer == 'n' : continue
+            elif answer == 'N' or answer == 'n' : 
+                os.system('cls')
+                continue
         except KeyboardInterrupt:
             sys.exit()
         except:
--- scan_method.py
+++ scan_method.py
@@ -14,6 +14,9 @@ def scan_method():
         try :  
             if res_option.headers['allow'] :
                 print(res_option.headers['allow'],"사용")
+                if res_option.headers['server'] :
+                    print("Server info:",res_option.headers['server'])
+                else : pass
         except :
             print("웹사이트 header 정보\n",res_option.headers)
             if res_head.status_code == 200 : print("HEAD 사용")
@@ -27,5 +30,15 @@ def scan_method():
             if res_delete.status_code == 200 : print("DELETE 사용")
             else : print("응답코드 ",res_head.status_code," DELETE 미사용")
     else :
-        print("사이트 헤더 정보를 읽어올 수 없음")
+        print("사이트 헤더 정보를 읽어올 수 없음\n")
+        if res_head.status_code == 200 : print("HEAD 사용")
+        else : print("응답코드",res_head.status_code," HEAD 미사용")
+        if res_get.status_code == 200 : print("GET 사용")
+        else : print("응답코드 ",res_head.status_code," GET 미사용")
+        if res_post.status_code == 200 : print("POST 사용")
+        else : print("응답코드 ",res_head.status_code," POST 미사용")
+        if res_put.status_code == 200 : print("PUT 사용")
+        else : print("응답코드 ",res_head.status_code," PUT 미사용")
+        if res_delete.status_code == 200 : print("DELETE 사용")
+        else : print("응답코드 ",res_head.status_code," DELETE 미사용")
    
